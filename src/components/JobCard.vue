6233cdb80fa13bf7cd7f88fa4a58017368972c53
con25 <63033573+con25@users.noreply.github.com>
2020-08-02 23:59:24 +0800

    Add files via upload

---
+++ src/components/JobCard.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <b-col>
+            <b-card
+                title="A card"
+                img-src="https://images.pexels.com/photos/1100946/pexels-photo-1100946.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+                img-alt="Job-info"
+                img-top
+                tag="article"
+                style="max-width:20rem"
+                class="mb-2"
+                footer="Footer Here"          
+            >
+                <b-card-text>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam quod aliquam placeat animi.
+                </b-card-text>
+                <b-button variant="primary">Search</b-button>
+            </b-card>
+            </b-col>
+    </div>
+</template>
+
+
+<script>
+export default {
+    
+}
+</script>
+
+<script>
+export default {
+    
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <b-container>
+      <b-row align-v="center">
+        <job-card></job-card>
+        <job-card></job-card>
+        <job-card></job-card>
+        <job-card></job-card>
+    </b-row>
+</b-container>
+  </div>
+</template>
+
+<script>
+import JobCard from '@/components/JobCard.vue';
+
+export default {
+  name: "Home",
+  components: {"job-card": JobCard}
+};
+</script>
